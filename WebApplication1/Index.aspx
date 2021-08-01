ea9222c56c8202d9ae3b87d051430366318cc8be
Yurii <yura9989@gmail.com>
2021-08-01 20:04:36 +0300

    ClearFields function added

--- WebApplication1/Index.aspx
+++ WebApplication1/Index.aspx
@@ -6,7 +6,16 @@
 <head runat="server">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title>Announcment Site</title>
+    <script type="text/javascript" src="/js/jquery-2.1.4.min.js"></script>
+    <script type="text/javascript" src="/js/jquery.maskedinput.js"></script>
+    <script src="/scripts/jquery-2.1.0.min.js"></script>
+    <script type="text/javascript">
+        $(function () {
+            $("#txtDateAdd").mask("999-999-9999");
+        });
+    </script>
 </head>
+    
 <body>
     <form id="form1" runat="server">
         <asp:HiddenField ID="hfProductID" runat="server" />
@@ -36,21 +45,24 @@
                         <asp:TextBox ID="txtDescription" runat="server"/>
                     </td>
                 </tr>
+
                 <tr>
                     <td>
                         <asp:Label Text="DateAdd"  runat="server"/>
                     </td>
                     <td colspan="2">
-                        <asp:TextBox ID="txtDateAdd" runat="server" ValidationGroup/>
+                        <asp:TextBox ID="txtDateAdd" runat="server"/><%-- MaximumValue="1/1/2050" MinimumValue="2000/1/1" Type="Date" Format="yyyy MM dd"/>--%>
+
                     </td>
                 </tr>
+
                 <tr>
                     <td></td>
                     <td colspan="3">
                         <asp:Button Text="Save" id="btnSave" runat="server" OnClick="Unnamed5_Click"/>
-                        <asp:Button Text="Delete" id="btnDelete" runat="server"/>
+                        <asp:Button Text="Delete" id="btnDelete" runat="server" OnClick="btnDelete_Click"/>
                         <asp:Button Text="Change" id="btnChange" runat="server"/>
-                        <asp:Button Text="Clear" id="btnClear" runat="server" />
+                        <asp:Button Text="Clear" id="btnClear" runat="server" OnClick="btnClear_Click" />
                     </td>
                 </tr>
 
--- WebApplication1/Index.aspx.cs
+++ WebApplication1/Index.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -19,7 +20,10 @@ namespace WebApplication1
         ";
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                clearFields();
+            }
         }
 
         protected void gvProduct_SelectedIndexChanged(object sender, EventArgs e)
@@ -29,13 +33,67 @@ namespace WebApplication1
 
         protected void Unnamed5_Click(object sender, EventArgs e)
         {
+            if(txtName.Text != "" || txtTitle.Text != "" || txtDescription.Text != "") { 
+                try { 
+                    using (MySqlConnection sqlCon = new MySqlConnection(connectionString))
+                    {
+                    
+                        sqlCon.Open();
+                        MySqlCommand sqlCmd = new MySqlCommand("posterAddorEdit", sqlCon);
+                        sqlCmd.CommandType = CommandType.StoredProcedure;
+                        sqlCmd.Parameters.AddWithValue("_announcementID", Convert.ToInt32(hfProductID.Value == "" ? "0" : hfProductID.Value));
+                        sqlCmd.Parameters.AddWithValue("_name", txtName.Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("_title", txtTitle.Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("_description", txtDescription.Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("_dateAdd", Convert.ToDateTime(txtDateAdd.Text));                                                  
+                        sqlCmd.ExecuteNonQuery();
+
+                        clearFields();
+
+                        lbSucessMessage.Text = "Успішно додано";
+                        clearFields();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    lbErrorMessage.Text = "Submited failed" + ex.Message;
+                    throw ex;
+                }
+            }
+            else
+            {
+                lbErrorMessage.Text = "Ви не ввели жодного поля";
+            }
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void clearFields()
+        {
+            txtName.Text = "";
+            txtTitle.Text = "";
+            txtDescription.Text = "";
+            txtDateAdd.Text = "";
+            btnSave.Text = "Save";
+            btnDelete.Enabled = false;
+            lbErrorMessage.Text = lbSucessMessage.Text = "";
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            clearFields();
+        }
+
+        void GridFild()
+        {
             using (MySqlConnection sqlCon = new MySqlConnection(connectionString))
             {
                 sqlCon.Open();
-                MySqlCommand sqlCmd = new MySqlCommand("posterAddorEdit", sqlCon);
-                sqlCmd.CommandType = CommandType.StoredProcedure;
-                sqlCmd.Parameters.AddWithValue("_announcementID", Convert.ToInt32(hfProductID.Value == "" ? "0" : hfProductID.Value));
-                //sqlCmd.Parameters.AddWithValue("_name", txt);
+                MySqlDataAdapter sqlDat = new MySqlDataAdapter();
+
             }
         }
     }
